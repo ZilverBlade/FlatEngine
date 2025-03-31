91e8d272917fadbd18e1f5586382e973496e885f
Tristan Fransen <t.a.w.d.fransen@student.tudelft.nl>
2025-03-31 10:32:37 +0200

    added files

---
+++ flatengine.cmake
@@ -0,0 +1,38 @@
+﻿set(SH_SRC "${CMAKE_SOURCE_DIR}/src/flatengine")
+file(GLOB_RECURSE ENDF6_SRC
+      "${SH_SRC}/*.h"
+      "${SH_SRC}/*.cpp")
+      
+add_executable("flatengine" ${ENDF6_SRC})
+
+foreach(_source IN ITEMS ${ENDF6_SRC})
+    get_filename_component(_source_path "${_source}" PATH)
+    string(REPLACE "${SH_SRC}" "" _group_path "${_source_path}")
+    string(REPLACE "/" "\\" _group_path "${_group_path}")
+    source_group("${_group_path}" FILES "${_source}")
+endforeach()
+
+set_target_properties("flatengine" PROPERTIES
+  CXX_STANDARD 17
+  CXX_STANDARD_REQUIRED YES
+  CXX_EXTENSIONS NO
+  POSITION_INDEPENDENT_CODE False
+  INTERPROCEDURAL_OPTIMIZATION False
+)
+
+target_include_directories("flatengine" PRIVATE 
+  ${CMAKE_SOURCE_DIR}/src
+)
+
+if (WIN32)
+set_property(TARGET "flatengine" 
+PROPERTY MSVC_RUNTIME_LIBRARY MultiThreaded$<$<CONFIG:Debug>:Debug>DLL
+)
+endif(WIN32)
+
+set_target_properties("flatengine" PROPERTIES
+  OUTPUT_NAME "flatengine"
+  ARCHIVE_OUTPUT_DIRECTORY ${OUTPUT_PATH}
+  LIBRARY_OUTPUT_DIRECTORY ${OUTPUT_PATH}
+  RUNTIME_OUTPUT_DIRECTORY ${OUTPUT_PATH}
+)
